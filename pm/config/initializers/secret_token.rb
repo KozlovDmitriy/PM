e4765642b838bd4fb073bf25a002eb1905e6b177
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-21 23:39:36 +0400

    Проект создан в rails

---
+++ pm/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pm::Application.config.secret_key_base = '83dbbe20fbb6a7555d15a5954c18736dff0c7c1f849cb001735000a6a5d156d200800ed35ef6addfaf79fc402f231b03f7761b0ec4054ad209464fbaf662df04'
