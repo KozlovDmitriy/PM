03947bea6dd517a6d619b1edfacfab5a184379f5
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-22 10:07:51 +0400

    Добавлена верстка детальной страницы консультанта.

--- pm/app/assets/javascripts/application.js
+++ pm/app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require docs.min
+//= require globalize
+//= require dx.chartjs
 //= require_tree .
--- pm/app/models/consultant.rb
+++ pm/app/models/consultant.rb
@@ -1,2 +1,13 @@
 class Consultant < ActiveRecord::Base
+
+  # Метод получения краткого имени консультанта.
+  def short_name
+    "#{family_name} #{first_name[0]}. #{second_name[0]}."
+  end
+
+  # Метод получения краткого имени консультанта.
+  def full_name
+    "#{family_name} #{first_name} #{second_name}"
+  end
+
 end
