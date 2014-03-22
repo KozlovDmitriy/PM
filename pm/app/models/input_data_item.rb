b75f07d1e05ad7decdcf530b03add8fbaec34a5f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-22 13:31:42 +0400

    Добавлена связь между введенными данныи и значениями параметров.

--- pm/app/models/input_data_item.rb
+++ pm/app/models/input_data_item.rb
@@ -1,4 +1,6 @@
 class InputDataItem < ActiveRecord::Base
   belongs_to :input_datum
   belongs_to :consultant
+
+  has_and_belongs_to_many :param_values
 end
--- pm/app/models/param_value.rb
+++ pm/app/models/param_value.rb
@@ -1,3 +1,5 @@
 class ParamValue < ActiveRecord::Base
   belongs_to :param
+
+  has_and_belongs_to_many :input_data_items
 end
