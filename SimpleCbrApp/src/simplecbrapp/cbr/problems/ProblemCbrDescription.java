827f34497366ffa73fbc11e9945e181c1e6a5e8d
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-24 22:36:55 +0400

    В класс описания прецедента при поиске проблем добавлены поля класса.

--- SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrDescription.java
+++ SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrDescription.java
@@ -1,10 +1,114 @@
 package simplecbrapp.cbr.problems;
 
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CaseComponent;
+import jcolibri.datatypes.Instance;
+
 /**
  * Класс описания прецедента при поиске проблем.
  * @author M. Navrotskiy
  * @version 1.0
  */
-public class ProblemCbrDescription {
+public class ProblemCbrDescription implements CaseComponent {
+    
+    /* Поля класса. */
+    /** Выполнение плана. */
+    private Instance implPlan;
+    /** Значение среднего чека. */
+    private Instance avCheck;
+    /** Количество позиций в чеке. */
+    private Instance itemsCount;
+    /** Общее количество чеков. */
+    private Instance totalChecksCount;
+    /** Проблема в работе персонала. */
+    private Instance problem;
+
+    /**
+     * Метод изменения общего количества чеков.
+     * @param totalChecksCount Значение общего количества чеков.
+     */
+    public void setTotalChecksCount(Instance totalChecksCount) {
+        this.totalChecksCount = totalChecksCount;
+    }
+
+    /**
+     * Метод изменения значения проблемы, 
+     * которая возникает в работе консультанта.
+     * @param problem Значение проблемы, 
+     * которая возникает в работе консультанта.
+     */
+    public void setProblem(Instance problem) {
+        this.problem = problem;
+    }
+
+    /**
+     * Метод измения значения количества позиций в чеке.
+     * @param itemsCount Значение количества позиций в чеке.
+     */
+    public void setItemsCount(Instance itemsCount) {
+        this.itemsCount = itemsCount;
+    }
+
+    /**
+     * Метод изменения значения выполнения плана.
+     * @param implPlan Значение выполненения плана.
+     */
+    public void setImplPlan(Instance implPlan) {
+        this.implPlan = implPlan;
+    }
+
+    /**
+     * Метод изменения значения среднего чека.
+     * @param avCheck Значение среднего чека.
+     */
+    public void setAvCheck(Instance avCheck) {
+        this.avCheck = avCheck;
+    }
+
+    /**
+     * Метод получения значения общего количества чеков.
+     * @return Значение общего количества чеков.
+     */
+    public Instance getTotalChecksCount() {
+        return totalChecksCount;
+    }
+
+    /**
+     * Метод получения проблемы, которая возникла в работе консультанта.
+     * @return Значение проблемы в работе консультанта.
+     */
+    public Instance getProblem() {
+        return problem;
+    }
+
+    /**
+     * Метод получения значения количества позиций в чеке.
+     * @return Значение количества позиций в чеке.
+     */
+    public Instance getItemsCount() {
+        return itemsCount;
+    }
+
+    /**
+     * Метод получения значения выполнения индивидуального плана.
+     * @return Значение выполнения индивидуального плана.
+     */
+    public Instance getImplPlan() {
+        return implPlan;
+    }
+
+    /**
+     * Метод получения значения среднего чека.
+     * @return Значение среднего чека.
+     */
+    public Instance getAvCheck() {
+        return avCheck;
+    }
+
+    @Override
+    public Attribute getIdAttribute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
