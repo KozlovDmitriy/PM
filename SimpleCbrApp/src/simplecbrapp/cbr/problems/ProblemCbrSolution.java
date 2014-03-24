a37d8da2dfc6b3d245117ac1ce222826a189f911
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-24 23:03:17 +0400

    Добавлен класс описания решения при поиске проблем.

---
+++ SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrSolution.java
@@ -0,0 +1,25 @@
+package simplecbrapp.cbr.problems;
+
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CaseComponent;
+import jcolibri.datatypes.Instance;
+
+/**
+ * Класс описания решения при поиске проблем.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class ProblemCbrSolution implements CaseComponent {
+    
+    /* Поля класса. */
+    /** Концепт. */
+    private Instance mainConcept;
+    /** Проблема. */
+    private Instance problem;
+
+    @Override
+    public Attribute getIdAttribute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
