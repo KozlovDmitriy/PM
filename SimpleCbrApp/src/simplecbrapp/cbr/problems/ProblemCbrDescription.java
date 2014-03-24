2eeedbcffc384601d66055ffa598e7c6c7416663
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-24 22:23:06 +0400

    Добавлен класс описания прецедента при поиске проблем.

---
+++ SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrDescription.java
@@ -0,0 +1,10 @@
+package simplecbrapp.cbr.problems;
+
+/**
+ * Класс описания прецедента при поиске проблем.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class ProblemCbrDescription {
+    
+}
