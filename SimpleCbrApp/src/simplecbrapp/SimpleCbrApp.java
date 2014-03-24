aaf93f09cca81977ea00dfee6ee073f3a07e6ccc
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-24 22:18:50 +0400

    Добавления каркас простого cbr приложения для построения основы взаимодействия между Rails и Java.

---
+++ SimpleCbrApp/src/simplecbrapp/SimpleCbrApp.java
@@ -0,0 +1,18 @@
+package simplecbrapp;
+
+/**
+ * Простое cbr приложения для построения основы взаимодействия Rails и Java.
+ * @author M. Navrotskiy.
+ * @version 1.0
+ */
+public class SimpleCbrApp {
+
+    /**
+     * Главная функция приложения.
+     * @param args Аргументы коммандной строки.
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
