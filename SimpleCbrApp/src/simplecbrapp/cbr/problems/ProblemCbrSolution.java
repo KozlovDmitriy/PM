af158bb97f8301b3f1c30cbd0d0a47291268001d
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-24 23:10:04 +0400

    В класс описания решения при поиске проблем, добавлены методы доступа к полям класса.

--- SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrSolution.java
+++ SimpleCbrApp/src/simplecbrapp/cbr/problems/ProblemCbrSolution.java
@@ -17,6 +17,38 @@ public class ProblemCbrSolution implements CaseComponent {
     /** Проблема. */
     private Instance problem;
 
+    /**
+     * Метод изменения значения проблемы.
+     * @param problem Новое значение проблемы.
+     */
+    public void setProblem(Instance problem) {
+        this.problem = problem;
+    }
+
+    /**
+     * Метод изменения значения главного концепта.
+     * @param mainConcept Значение главного концепта.
+     */
+    public void setMainConcept(Instance mainConcept) {
+        this.mainConcept = mainConcept;
+    }
+
+    /**
+     * Метод получения значения проблемы.
+     * @return Значение проблемы.
+     */
+    public Instance getProblem() {
+        return problem;
+    }
+
+    /**
+     * Метод получения значения главного концепта.
+     * @return Значение главного концепта.
+     */
+    public Instance getMainConcept() {
+        return mainConcept;
+    }
+
     @Override
     public Attribute getIdAttribute() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
