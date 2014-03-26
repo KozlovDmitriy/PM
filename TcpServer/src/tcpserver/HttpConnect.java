edce2e3777acf0e058315b89572dc5d3a3f092e0
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-26 07:20:13 +0400

    В класс создания сокетного соединения, добавлен конструктор с параметром.

--- TcpServer/src/tcpserver/HttpConnect.java
+++ TcpServer/src/tcpserver/HttpConnect.java
@@ -8,10 +8,21 @@ import java.net.Socket;
  * @author M.Navrotskiy
  * @version 1.0
  */
-public class HttpConnect {
+public class HttpConnect extends Thread {
     
     /* Поля класса. */
     /** Сокет. */
     private Socket socket;
-    
+
+    /**
+     * Конструктор с параметром.
+     * @param socket Сокет по которому организуется связь.
+     */
+    public HttpConnect(Socket socket) {
+        
+        this.socket = socket;
+        this.setPriority(Thread.NORM_PRIORITY - 1);
+        this.start();
+    }
+       
 }
