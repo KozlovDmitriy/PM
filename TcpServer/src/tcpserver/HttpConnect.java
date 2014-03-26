489c4a4d1123a2fedb1fa11082bb1980c6777267
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-26 07:16:05 +0400

    Добавлен класс создания сокетного соединения.

---
+++ TcpServer/src/tcpserver/HttpConnect.java
@@ -0,0 +1,17 @@
+
+package tcpserver;
+
+import java.net.Socket;
+
+/**
+ * Класс создания сокетного соединения.
+ * @author M.Navrotskiy
+ * @version 1.0
+ */
+public class HttpConnect {
+    
+    /* Поля класса. */
+    /** Сокет. */
+    private Socket socket;
+    
+}
