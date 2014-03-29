7dcb0e8509fd301a6fcbd427b0466995afff3eec
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-03-29 14:02:23 +0400

    Добавлены константы режима для сервера.

--- pm/lib/tcp_client.rb
+++ pm/lib/tcp_client.rb
@@ -11,10 +11,19 @@ class TcpClient
 
   # Константы связи с Java сервером.
   # Конец общения.
+  END_CONNECTION = 0x01
   # Блок параметров работы консультантов.
+  PARAMS_BLOCK_START = 0x02 # Начало.
+  PARAMS_BLOCK_END = 0x03 # Конец.
   # Блок проблем в работе консультантов.
+  PROBLEM_BLOCK_START = 0x04 # Начало.
+  PROBLEM_BLOCK_END = 0x05 # Конец.
   # Ответ в виде множества проблем.
+  RESPONSE_PROBLEM_BLOCK_START = 0x06 # Начало.
+  RESPONSE_PROBLEM_BLOCK_END = 0x07 # Конец.
   # Ответ в виде множества рекомендаций.
+  RESPONSE_SOLUTION_BLOCK_START = 0x08 # Начало.
+  RESPONSE_SOLUTION_BLOCK_END = 0x09 # Конец.
 
   # Конструктор.
   def initialize port, host
