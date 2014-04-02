314ad754535b8e33e854bc3ed1a68f61d436891b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-02 23:36:14 +0400

    Код запуска CBR добавлен в главный класс приложения.

--- SimpleCbrApp/src/simplecbrapp/SimpleCbrApp.java
+++ SimpleCbrApp/src/simplecbrapp/SimpleCbrApp.java
@@ -1,5 +1,16 @@
 package simplecbrapp;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jcolibri.cbrcore.CBRCase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.datatypes.Instance;
+import jcolibri.exception.ExecutionException;
+import jcolibri.exception.OntologyAccessException;
+import simplecbrapp.cbr.problems.ProblemCbrApplication;
+import simplecbrapp.cbr.problems.ProblemCbrDescription;
+import simplecbrapp.cbr.problems.ProblemCbrSolution;
+
 /**
  * Простое cbr приложения для построения основы взаимодействия Rails и Java.
  * @author M. Navrotskiy.
@@ -13,6 +24,29 @@ public class SimpleCbrApp {
      */
     public static void main(String[] args) {
         
+        try {
+            String result = "";
+            
+            ProblemCbrApplication app = ProblemCbrApplication.getInstance();
+            app.configure();
+            app.preCycle();
+            
+            ProblemCbrDescription description = 
+                    new ProblemCbrDescription(new Instance(""), new Instance(""), 
+                            new Instance(""), new Instance(""), new Instance(""), 
+                            new Instance(""));
+            
+            CBRQuery query = new CBRQuery();
+            query.setDescription(description);
+            app.cycle(query);
+            CBRCase c = app.getResult();
+            ProblemCbrSolution solution = (ProblemCbrSolution) c.getSolution()
+            
+        } catch (ExecutionException ex) {
+            Logger.getLogger(SimpleCbrApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (OntologyAccessException ex) {
+            Logger.getLogger(SimpleCbrApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
