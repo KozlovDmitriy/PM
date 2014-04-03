3e71352536902fffbd297e973a9359d01a15286c
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-04 00:41:01 +0400

    В класс описания решения при поиске рекомендаций добавлены конструкторы.

--- SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrSolution.java
+++ SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrSolution.java
@@ -17,6 +17,25 @@ public class SolutionCbrSolution implements CaseComponent {
     /** Рекомендация. */
     private Instance solution;
 
+    /**
+     * Конструктор с параметрами.
+     * @param mainConcept Значение главного концепта.
+     * @param solution Значение рекомендации.
+     */
+    public SolutionCbrSolution(Instance mainConcept, Instance solution) {
+        this.mainConcept = mainConcept;
+        this.solution = solution;
+    }
+
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует все поля в null.
+     */
+    public SolutionCbrSolution() {
+        this.mainConcept = null;
+        this.solution = null;
+    }
+
     @Override
     public Attribute getIdAttribute() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
