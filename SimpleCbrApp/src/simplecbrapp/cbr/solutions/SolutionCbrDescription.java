17561b48843f3eae72699f26a97d3e31da6dea41
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-04 00:53:40 +0400

    Добавлен класс описания прецедента при поиске рекомендаций.

---
+++ SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrDescription.java
@@ -0,0 +1,18 @@
+package simplecbrapp.cbr.solutions;
+
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CaseComponent;
+
+/**
+ * Класс описания прецедента при поиске рекомендаций.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class SolutionCbrDescription implements CaseComponent {
+
+    @Override
+    public Attribute getIdAttribute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
