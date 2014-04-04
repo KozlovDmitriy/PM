8b4bc5ed794aa8921a0f31e9d9dd2ee92326152e
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-04 19:23:48 +0400

    Добавлен класс CBR приложения для поиска рекомендаций.

---
+++ SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrApplication.java
@@ -0,0 +1,35 @@
+package simplecbrapp.cbr.solutions;
+
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.exception.ExecutionException;
+
+/**
+ * Класс CBR приложения для поиска рекомендаций.
+ * @author M. Navrotskiy
+ * @version 1.0
+ */
+public class SolutionCbrApplication implements StandardCBRApplication {
+
+    @Override
+    public void configure() throws ExecutionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CBRCaseBase preCycle() throws ExecutionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cycle(CBRQuery query) throws ExecutionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void postCycle() throws ExecutionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
