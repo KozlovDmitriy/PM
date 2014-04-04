a3055dbe8d82c4693be3934a5e8f0ef72e13be75
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-04 22:47:02 +0400

    В класс CBR приложения для поиска рекомендаций добавлены поля класса.

--- SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrApplication.java
+++ SimpleCbrApp/src/simplecbrapp/cbr/solutions/SolutionCbrApplication.java
@@ -1,9 +1,13 @@
 package simplecbrapp.cbr.solutions;
 
+import java.util.Collection;
 import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCase;
 import jcolibri.cbrcore.CBRCaseBase;
 import jcolibri.cbrcore.CBRQuery;
+import jcolibri.connector.OntologyConnector;
 import jcolibri.exception.ExecutionException;
+import jcolibri.method.retrieve.RetrievalResult;
 
 /**
  * Класс CBR приложения для поиска рекомендаций.
@@ -12,6 +16,22 @@ import jcolibri.exception.ExecutionException;
  */
 public class SolutionCbrApplication implements StandardCBRApplication {
 
+    /* Поля класса. */
+    /** Запрос в базу прецедентов. */
+    private CBRQuery query;
+    /** Результат работы CBR. */
+    private Collection<RetrievalResult> eval;
+    /** База прецедентов. */
+    private CBRCaseBase caseBase;
+    /** Множество близкий прецедентов. */
+    private Collection<CBRCase> selectedCase;
+    /** Соединение с онтологией. */
+    private OntologyConnector connector;
+    /** Объект класса. */
+    private SolutionCbrApplication app;
+    /** Ближайший прецедент. */
+    private CBRCase result;
+    
     @Override
     public void configure() throws ExecutionException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
