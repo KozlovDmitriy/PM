e6d43c5884c18468a6ac4a844eb901a08b9358f0
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-08 08:35:11 +0400

    В класс Rails клиента добавлены константы режимов чтения параметров из онтологии.

--- pm/app/controllers/analysis_controller_controller.rb
+++ pm/app/controllers/analysis_controller_controller.rb
@@ -1,4 +1,8 @@
 class AnalysisControllerController < ApplicationController
   def index
+    #require_relative '../../lib/tcp_client'
+    #client = TcpClient.new 50125, 'localhost'
+    # read params
+    #client.close
   end
 end
