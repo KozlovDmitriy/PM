47f51245d1ab5aaa5ac69e5a461f2d20501498e2
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-04-19 22:14:35 +0400

    Неудачная попытка настройки тестов.

--- pm/test/controllers/consultants_controller_test.rb
+++ pm/test/controllers/consultants_controller_test.rb
@@ -1,49 +1,55 @@
 require 'test_helper'
 
 class ConsultantsControllerTest < ActionController::TestCase
-  setup do
-    @consultant = consultants(:one)
-  end
+  #setup do
+  #  @consultant = consultants(:consultant_1)
+  #end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:consultants)
+    assert_select 'h1', 'Список консультантов'
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create consultant" do
-    assert_difference('Consultant.count') do
-      post :create, consultant: { family_name: @consultant.family_name, first_name: @consultant.first_name, second_name: @consultant.second_name }
-    end
-
-    assert_redirected_to consultant_path(assigns(:consultant))
-  end
-
-  test "should show consultant" do
-    get :show, id: @consultant
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @consultant
-    assert_response :success
-  end
-
-  test "should update consultant" do
-    patch :update, id: @consultant, consultant: { family_name: @consultant.family_name, first_name: @consultant.first_name, second_name: @consultant.second_name }
-    assert_redirected_to consultant_path(assigns(:consultant))
-  end
-
-  test "should destroy consultant" do
-    assert_difference('Consultant.count', -1) do
-      delete :destroy, id: @consultant
-    end
-
-    assert_redirected_to consultants_path
-  end
+  #test 'should get new' do
+  #  get :new
+  #  assert_response :success
+  #  assert_select 'h1', 'Добавить нового консультанта'
+  #end
+  #
+  ##test "should create consultant" do
+  ##  assert_difference('Consultant.count') do
+  ##    post :create, consultant: { family_name: @consultant.family_name, first_name: @consultant.first_name, second_name: @consultant.second_name }
+  ##  end
+  ##
+  ##  assert_redirected_to consultant_path(assigns(:consultant))
+  ##end
+  #
+  #test "should show consultant" do
+  #  #get :show, id: @consultant
+  #  get :show, :id => 1
+  #  assert_response :success
+  #  assert_select 'h1', /История работы консультанта: [А-Я][а-я]+\s[А-Я][а-я]+\s\.[А-Я][а-я]\./
+  #end
+  #
+  #test "should get edit" do
+  #  #get :edit, id: @consultant
+  #  get :edit, :id => 1
+  #  assert_response :success
+  #  assert_select 'h1', 'Изменить консультанта'
+  #end
+  #
+  ##test "should update consultant" do
+  ##  patch :update, id: @consultant, consultant: { family_name: @consultant.family_name, first_name: @consultant.first_name, second_name: @consultant.second_name }
+  ##  assert_redirected_to consultant_path(assigns(:consultant))
+  ##end
+  #
+  ###test "should destroy consultant" do
+  ###  assert_difference('Consultant.count', -1) do
+  ###    delete :destroy, id: @consultant
+  ###  end
+  ##
+  ##  assert_redirected_to consultants_path
+  ##end
 end
--- pm/test/controllers/input_data_controller_test.rb
+++ pm/test/controllers/input_data_controller_test.rb
@@ -1,49 +1,49 @@
 require 'test_helper'
 
 class InputDataControllerTest < ActionController::TestCase
-  setup do
-    @input_datum = input_data(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:input_data)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create input_datum" do
-    assert_difference('InputDatum.count') do
-      post :create, input_datum: { date: @input_datum.date }
-    end
-
-    assert_redirected_to input_datum_path(assigns(:input_datum))
-  end
-
-  test "should show input_datum" do
-    get :show, id: @input_datum
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @input_datum
-    assert_response :success
-  end
-
-  test "should update input_datum" do
-    patch :update, id: @input_datum, input_datum: { date: @input_datum.date }
-    assert_redirected_to input_datum_path(assigns(:input_datum))
-  end
-
-  test "should destroy input_datum" do
-    assert_difference('InputDatum.count', -1) do
-      delete :destroy, id: @input_datum
-    end
-
-    assert_redirected_to input_data_path
-  end
+  #setup do
+  #  @input_datum = input_data(:one)
+  #end
+  #
+  #test "should get index" do
+  #  get :index
+  #  assert_response :success
+  #  assert_not_nil assigns(:input_data)
+  #end
+  #
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
+  #
+  #test "should create input_datum" do
+  #  assert_difference('InputDatum.count') do
+  #    post :create, input_datum: { date: @input_datum.date }
+  #  end
+  #
+  #  assert_redirected_to input_datum_path(assigns(:input_datum))
+  #end
+  #
+  #test "should show input_datum" do
+  #  get :show, id: @input_datum
+  #  assert_response :success
+  #end
+  #
+  #test "should get edit" do
+  #  get :edit, id: @input_datum
+  #  assert_response :success
+  #end
+  #
+  #test "should update input_datum" do
+  #  patch :update, id: @input_datum, input_datum: { date: @input_datum.date }
+  #  assert_redirected_to input_datum_path(assigns(:input_datum))
+  #end
+  #
+  #test "should destroy input_datum" do
+  #  assert_difference('InputDatum.count', -1) do
+  #    delete :destroy, id: @input_datum
+  #  end
+  #
+  #  assert_redirected_to input_data_path
+  #end
 end
