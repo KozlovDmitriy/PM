3d4bbd68d79b26de72f92bb531e97971484c0edb
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-05-01 11:46:03 +0400

    В класс формирования файла-отчета добавлен конструктор.

--- pm/lib/write_excel.rb
+++ pm/lib/write_excel.rb
@@ -3,5 +3,13 @@ require 'roo'
 
 # Класс записи данных консультантов в excel файл.
 class WriteExcel
-  
+
+  # Конструктор класса.
+  # @param file_name [String] - имя файла.
+  def initialize file_name
+    @file_name = file_name
+    @file = Roo::OpenOffice.new @file_name
+    @file.default_sheet = @file.sheets.first
+  end
+
 end
