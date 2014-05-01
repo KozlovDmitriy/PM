021cf8f0c348b084333820e927cdb65cd49d77e0
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-05-01 16:34:46 +0400

    Добавлен вывод данных в просмотр результатов анализа при формировании отчета.

--- pm/app/models/analysis.rb
+++ pm/app/models/analysis.rb
@@ -1,7 +1,23 @@
 class Analysis < ActiveRecord::Base
 
   def array
-    
+    _id = 19
+    result = []
+    Consultant.all.each do |consultant|
+      unless ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => [1, 2, 3, 4, 5, 6]).nil?
+        hash = {
+            :name => consultant.short_name,
+            :ind_plan => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 1).value,
+            :impl_plan => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 2).value,
+            :impl => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 3).value,
+            :av_check => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 4).value,
+            :items_count => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 5).value,
+            :total_checks_count => ParamValue.find_by(:consultant_id => consultant.id, :date_id => _id, :param_id => 6).value
+        }
+        result.push hash
+      end
+    end
+    result
   end
 
 end
