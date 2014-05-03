d374d05de5cc25fbcae9c283dd74987a1d2c9785
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-05-03 17:37:32 +0400

    Реализован полный разбор ods файла и его сохранение в бд.

--- pm/app/controllers/input_data_controller.rb
+++ pm/app/controllers/input_data_controller.rb
@@ -22,18 +22,44 @@ class InputDataController < ApplicationController
 
     require_relative '../../lib/read_excel'
     ods = ReadExcel.new filename
+    File.open('debug.txt', 'a') { |file| file.write ods.get_params.to_yaml }
     
     analise = Analysis.new :date => Date.today
     analise.save
     ods.get_params.each do |item|
       
       consultant_family = item[:fio].match(/^[А-Я][а-я]+/).to_s
-      pv = ParamValue.new :param_id => Param.find_by(:name => 'Индивидуальный план').id, 
+      c_id = Consultant.find_by(:family_name => consultant_family).id
+      ParamValue.create :param_id => Param.find_by(:name => 'Индивидуальный план').id,
                           :value => item[:ind_plan],
                           :date_id => analise.id,
-                          :consultant_id => Consultant.find_by(:family_name => consultant_family).id
-      
-      File.open('debug.txt', 'a') { |file| file.write pv.to_yaml }
+                          :consultant_id => c_id
+
+      ParamValue.create :param_id => 2,
+                        :value => item[:impl_plan],
+                        :date_id => analise.id,
+                        :consultant_id => c_id
+
+      ParamValue.create :param_id => 3,
+                        :value => item[:impl],
+                        :date_id => analise.id,
+                        :consultant_id => c_id
+
+      ParamValue.create :param_id => 4,
+                        :value => item[:av_check],
+                        :date_id => analise.id,
+                        :consultant_id => c_id
+
+      ParamValue.create :param_id => 5,
+                        :value => item[:items],
+                        :date_id => analise.id,
+                        :consultant_id => c_id
+
+      ParamValue.create :param_id => 6,
+                        :value => item[:total_checks],
+                        :date_id => analise.id,
+                        :consultant_id => c_id
+
     end
 
     render :json => ods.get_params
