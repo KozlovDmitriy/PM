e8274c5d8b215604a78cb50aaf301c2902a7499a
M. Navrotskiy <m.navrotskiy@gmail.com>
2014-05-12 22:04:53 +0400

    Добавлен класс простой проблемы.

--- TcpServer/src/ontology/model/SimpleProblem.java
+++ TcpServer/src/ontology/model/SimpleProblem.java
@@ -15,6 +15,30 @@ public class SimpleProblem {
     private String URI;
 
     /**
+     * Метод изменения значения uri в онтологии.
+     * @param URI Новое значение uri в онтологии.
+     */
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+
+    /**
+     * Метод изменения значения описания проблемы.
+     * @param description Новое значение описания проблемы.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Метод получения URI проблемы в онтологии.
+     * @return uri проблемы в онтологии.
+     */
+    public String getURI() {
+        return URI;
+    }
+
+    /**
      * Метод получения значения описания проблемы.
      * @return Значение описания проблемы.
      */
