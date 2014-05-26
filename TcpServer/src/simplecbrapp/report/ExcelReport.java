e94310b410020c464ed105037af09168d685c95d
M. Navrotskiy <m.navrotskiy@gmail.com>
2014-05-26 22:49:51 +0400

    В класс работы со сводным отчетом добавлены поля класса.

--- TcpServer/src/simplecbrapp/report/ExcelReport.java
+++ TcpServer/src/simplecbrapp/report/ExcelReport.java
@@ -1,11 +1,25 @@
 
 package simplecbrapp.report;
 
+import java.io.FileInputStream;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 /**
  * Класс чтения и формирования сводного отчета.
  * @author M. Navrotskiy
  * @version 1.0
  */
 public class ExcelReport {
+    
+    /* Поля класса. */
+    /** Имя файла - отчета. */
+    private String filename;
+    /** Поток чтения. */
+    private FileInputStream fis;
+    /** Книга файла. */
+    private HSSFWorkbook wb;
+    /** Таблица. */
+    private HSSFSheet sheet;
     
 }
