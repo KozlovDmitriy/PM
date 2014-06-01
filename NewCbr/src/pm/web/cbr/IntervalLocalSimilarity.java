254451b81fd2576fae29e901ca36d85aef969939
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-06-01 12:09:07 +0400

    В класс локльной интервальной меры сходства добавлен метод сравнения прецедентов.

--- NewCbr/src/pm/web/cbr/IntervalLocalSimilarity.java
+++ NewCbr/src/pm/web/cbr/IntervalLocalSimilarity.java
@@ -1,5 +1,6 @@
 package pm.web.cbr;
 
+import jcolibri.datatypes.Instance;
 import jcolibri.exception.NoApplicableSimilarityFunctionException;
 import jcolibri.method.retrieve.NNretrieval.similarity.LocalSimilarityFunction;
 
@@ -9,8 +10,27 @@ import jcolibri.method.retrieve.NNretrieval.similarity.LocalSimilarityFunction;
  * @version 1.0
  */
 public class IntervalLocalSimilarity implements LocalSimilarityFunction {
+
+    /**
+     * Метод сравнения прецедентов.
+     * @param caseObject Прецедент из бд.
+     * @param queryObject Запрос.
+     * @return Результат сравнения.
+     * @throws NoApplicableSimilarityFunctionException
+     */
     @Override
-    public double compute(Object o, Object o2) throws NoApplicableSimilarityFunctionException {
+    public double compute(Object caseObject, Object queryObject) throws NoApplicableSimilarityFunctionException {
+
+        if ((caseObject == null) || (queryObject == null))
+            return 0;
+        if (!(caseObject instanceof Instance))
+            throw new jcolibri.exception.NoApplicableSimilarityFunctionException(this.getClass(), caseObject.getClass());
+        if (!(queryObject instanceof DescriptionQuery))
+            throw new jcolibri.exception.NoApplicableSimilarityFunctionException(this.getClass(), queryObject.getClass());
+
+        Instance c = (Instance) caseObject;
+        DescriptionQuery query = (DescriptionQuery) queryObject;
+
         return 0;
     }
 
