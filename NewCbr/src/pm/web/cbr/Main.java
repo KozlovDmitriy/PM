2e1bd586021912435a030327a06ff76c15fd299b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-06-01 11:29:59 +0400

    Добавлен тестовый код генерации html отчетов.

--- NewCbr/src/pm/web/cbr/Main.java
+++ NewCbr/src/pm/web/cbr/Main.java
@@ -1,8 +1,38 @@
 package pm.web.cbr;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Velocity.init();
+
+        VelocityContext context = new VelocityContext();
+
+        context.put("name", "Velocity");
+
+        Template template = null;
+
+        template = Velocity.getTemplate("out.vm");
+
+        StringWriter writer = new StringWriter();
+
+        template.merge(context, writer);
+
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("out.html"));
+            out.write(writer.toString());
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
