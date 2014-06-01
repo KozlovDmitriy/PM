2683e8fcb60107855cb6103c14be2072aab2090a
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-06-01 12:23:30 +0400

    В класс создания настройки CBR добавлен метод для первого эксперимента.

--- NewCbr/src/pm/web/cbr/CreateSimilarityConfig.java
+++ NewCbr/src/pm/web/cbr/CreateSimilarityConfig.java
@@ -1,9 +1,63 @@
 package pm.web.cbr;
 
+import jcolibri.cbrcore.Attribute;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.Equal;
+import pm.web.cbr.similarity.IntervalLocalSimilarity;
+
 /**
  * Класс создания разных настроек для CBR с разными локальными мерами сходства.
  * @author Mikhail Navrotskiy
  * @version 1.0
  */
 public class CreateSimilarityConfig {
+
+    /**
+     * Эксперимент 1 - использование своей меры близости.
+     * @return Настройка для CBR.
+     */
+    public static NNConfig experiment_1() {
+
+        NNConfig result = new NNConfig();
+        Attribute attribute;
+        Double weight = 1.0;
+
+        // avCheck
+        attribute = new Attribute("avCheck", Description.class);
+        result.addMapping(attribute, new IntervalLocalSimilarity());
+        result.setWeight(attribute, weight);
+
+        // itemsCount
+        attribute = new Attribute("itemsCount", Description.class);
+        result.addMapping(attribute, new IntervalLocalSimilarity());
+        result.setWeight(attribute, weight);
+
+        // checksCount
+        attribute = new Attribute("checksCount", Description.class);
+        result.addMapping(attribute, new IntervalLocalSimilarity());
+        result.setWeight(attribute, weight);
+
+        // impl
+        attribute = new Attribute("impl", Description.class);
+        result.addMapping(attribute, new IntervalLocalSimilarity());
+        result.setWeight(attribute, weight);
+
+        // implPlan
+        attribute = new Attribute("implPlan", Description.class);
+        result.addMapping(attribute, new IntervalLocalSimilarity());
+        result.setWeight(attribute, weight);
+
+        // sickList
+        attribute = new Attribute("sickList", Description.class);
+        result.addMapping(attribute, new Equal());
+        result.setWeight(attribute, weight);
+
+        // vacation
+        attribute = new Attribute("vacation", Description.class);
+        result.addMapping(attribute, new Equal());
+        result.setWeight(attribute, weight);
+
+        return result;
+    }
+
 }
