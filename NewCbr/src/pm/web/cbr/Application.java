a6a504904caec3a8384fb4a3b330ce0cdde386f9
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-06-01 11:35:21 +0400

    Добавлен класс CBR приложения.

---
+++ NewCbr/src/pm/web/cbr/Application.java
@@ -0,0 +1,55 @@
+package pm.web.cbr;
+
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCase;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.connector.OntologyConnector;
+import jcolibri.exception.ExecutionException;
+import jcolibri.method.retrieve.RetrievalResult;
+
+import java.util.Collection;
+
+/**
+ * Класс CBR приложения.
+ * @author Mikhail Navrotskiy
+ * @version 1.0
+ */
+public class Application implements StandardCBRApplication {
+
+    /* Поля класса. */
+    /** Запрос. */
+    private CBRQuery query;
+    /** Результат CBR цикла. */
+    private Collection<RetrievalResult> eval;
+    /** БД прецедентов. */
+    private CBRCaseBase caseBase;
+    /** Выбранные прецеденты. */
+    private Collection<CBRCase> selectedCase;
+    /** Коннектор с онтологией. */
+    private OntologyConnector connector;
+    /** Экземпляр класса. */
+    private static Application instance;
+    /** Значение CBR (конкретный case). */
+    private CBRCase result;
+
+    @Override
+    public void configure() throws ExecutionException {
+
+    }
+
+    @Override
+    public CBRCaseBase preCycle() throws ExecutionException {
+        return null;
+    }
+
+    @Override
+    public void cycle(CBRQuery cbrQuery) throws ExecutionException {
+
+    }
+
+    @Override
+    public void postCycle() throws ExecutionException {
+
+    }
+}
