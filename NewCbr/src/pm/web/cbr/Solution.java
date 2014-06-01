4009ad94e2d3f23c5a4c92abba731e33dbb73c59
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2014-06-01 11:04:19 +0400

    В класс case solution добавлены методы доступа к полям класса.:

--- NewCbr/src/pm/web/cbr/Solution.java
+++ NewCbr/src/pm/web/cbr/Solution.java
@@ -19,6 +19,54 @@ public class Solution implements CaseComponent {
     /** Множество рекомендаций. */
     private Instance solutions;
 
+    /**
+     * Метод получения значения главного концепта.
+     * @return Значение главного концепта.
+     */
+    public Instance getMainConcept() {
+        return mainConcept;
+    }
+
+    /**
+     * Метод изменения значения главного концепта.
+     * @param mainConcept Новое значение главного концепта.
+     */
+    public void setMainConcept(Instance mainConcept) {
+        this.mainConcept = mainConcept;
+    }
+
+    /**
+     * Метод получения значения проблем.
+     * @return Значение проблем.
+     */
+    public Instance getProblems() {
+        return problems;
+    }
+
+    /**
+     * Метод изменения значения проблем.
+     * @param problems Новое значение проблем.
+     */
+    public void setProblems(Instance problems) {
+        this.problems = problems;
+    }
+
+    /**
+     * Метод получения значения рекомендаций.
+     * @return Значение рекомендациий.
+     */
+    public Instance getSolutions() {
+        return solutions;
+    }
+
+    /**
+     * Метод изменения значения рекомендаций.
+     * @param solutions Новое значение рекомендаций.
+     */
+    public void setSolutions(Instance solutions) {
+        this.solutions = solutions;
+    }
+
     @Override
     public Attribute getIdAttribute() {
         return null;
